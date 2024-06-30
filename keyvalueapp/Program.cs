c27d18ee8555ebeb5c977f3f5e50ea53f0d3273e
santosh131 <santosh_janapala@hotmail.com>
2024-06-29 22:33:57 -0400

    removed sensitive information

--- keyvalueapp/Program.cs
+++ keyvalueapp/Program.cs
@@ -8,9 +8,9 @@ class Program
         Console.WriteLine("-----------Retrieve Secret Value from Azure KeyVault-----------");
         var keyValutName = "santoshkeyvault2";
         var keyVaultUri = $"https://{keyValutName}.vault.azure.net/";
-        var tenantId="970f0b6d-e74a-4d60-9dbf-3541e4007161";
-        var clientId="8030654a-d873-4d9e-aec7-c5e88b1b63fe";
-        var clientSecret="BPi8Q~_N6MVF2etkemvazUicsLOz81W7i46x-auV";
+        var tenantId="<tenant id from app registrations>";
+        var clientId="<client id from app registrations>";
+        var clientSecret="<client secret from app registrations>";
 
         var credential = new ClientSecretCredential(tenantId, clientId, clientSecret);
         
